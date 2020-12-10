8fc41e3b9174f9938ec8ff85faf1e09dc7c10b9f
dxy <958023205@qq.com>
2020-12-10 15:13:43 +0800

    第1次

---
+++ GetTest01/src/main/java/com/dxy/git/TestGit.java
@@ -0,0 +1,9 @@
+package com.dxy.git;
+
+public class TestGit {
+    public static void main(String[] args) {
+        System.out.println("1");
+        System.out.println("2");
+        System.out.println("3");
+    }
+}
